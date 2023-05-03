7bc73663f7643fc64d0424fbed268bababb42d38
Jbnado <73846881+Jbnado@users.noreply.github.com>
2023-05-03 08:56:20 -0300

    Create brackets-open-close.js

---
+++ brackets-open-close.js
@@ -0,0 +1,19 @@
+You're working with an intern that keeps coming to you with JavaScript code that won't run because the braces, brackets, and parentheses are off. To save you both some time, you decide to write a braces/brackets/parentheses validator.
+
+Let's say:
+
+'(', '{', '[' are called "openers."
+')', '}', ']' are called "closers."
+Write an efficient function that tells us whether or not an input string's openers and closers are properly nested.
+
+Examples:
+
+"{ [ ] ( ) }" should return true
+"{ [ ( ] ) }" should return false
+"{ [ }" should return false
+Gotchas
+Simply making sure each opener has a corresponding closer is not enough—we must also confirm that they are correctly ordered.
+
+For example, "{ [ ( ] ) }" should return false, even though each opener can be matched to a closer.
+
+We can do this in O(n) time and space. One iteration is all we need!
